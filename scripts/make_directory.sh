14256f2275b92065407abd09a2a35003dae25f20
EC2 Default User <ec2-user@ip-10-0-1-94.ap-northeast-1.compute.internal>
2024-11-06 06:27:36 +0000

    Update make_directory.sh to check for existing directory and add logging

--- scripts/make_directory.sh
+++ scripts/make_directory.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
-# デプロイ先ディレクトリが存在しない場合は作成
+echo "Checking if directory exists..." >> /tmp/deploy.log
 if [ ! -d "/home/ec2-user/simple-web-app" ]; then
-  mkdir -p /home/ec2-user/simple-web-app
+  echo "Creating directory..." >> /tmp/deploy.log
+  mkdir -p /home/ec2-user/simple-web-app >> /tmp/deploy.log 2>&1
+  echo "Directory created." >> /tmp/deploy.log
+else
+  echo "Directory already exists." >> /tmp/deploy.log
 fi
