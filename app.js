0b6d2b25d06731efaed57dd5a2adf2c481655dfc
EC2 Default User <ec2-user@ip-10-0-1-94.ap-northeast-1.compute.internal>
2024-11-07 12:31:56 +0000

    update package

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 // app.js
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 80;  // ポートを3000から80に変更
 
 app.get('/', (req, res) => {
   res.send('Hello, CI/CD World!');
 });
 
-// '0.0.0.0'でIPv4アドレスでリッスンするように変更
 app.listen(port, '0.0.0.0', () => {
   console.log(`App running on http://localhost:${port}`);
 });
